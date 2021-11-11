35631534aa7a6b593aefa62149c4d69140ad4eeb
harshpatel100 <90831761+harshpatel100@users.noreply.github.com>
2021-11-11 08:55:18 +0530

    asa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get("",(req,res)=>{
-  res.send("hellow world!!")
+  res.write("hellow world!!")
 })
 app.listen(3000,()=>{
   console.log("ya")
